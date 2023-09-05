db45b54f5f9ac9d31edaafa628a5cd5de367888e
yashbalan <124858190+yashbalan@users.noreply.github.com>
2023-09-05 16:44:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ june_vehicles_file_path = 'KM Data/Vehicles-Daily-Report-01-Jun-2023-12-00-AM-to
 df_vehicles_june = pd.DataFrame(load_monthly_data(june_vehicles_file_path))
 
 # Load July vehicles data
-july_vehicles_file_path = 'KM Data/Vehicles-Daily-Report-01-Jul-2023-12-00-AM-to-31-Jul-2023-11-59-PM.xlsx - Vehicle Daily Report.csv'
+july_vehicles_file_path = 'KM Data/Vehicles-Daily-Report-01-Jul-2023-12-00-AM-to-31-Jul-2023-11-59-PM.csv'
 df_vehicles_july = pd.DataFrame(load_monthly_data(july_vehicles_file_path))
 
 # Concatenate June and July data for df_vehicles_month
